c7a9905d9ed42af984de4fc160e464aab0b06bca
tomeichlersmith <eichl008@umn.edu>
2022-03-16 13:57:16 -0500

    more notes and patches

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Production container built ~7 months ago, pulling down to UMN for testing.
 ```
 cd ${LDMX_BASE}
 singularity build ldmx_local_sim-technique-dmg4.sif docker://tomeichlersmith/ldmx-sw:sim-technique-dmg4
+ldmx use local sim-technique-dmg4
 ```
 
 ## Documentation
--- ana/analysis.py
+++ ana/analysis.py
@@ -16,20 +16,20 @@ def get_dark_brem_products(event) :
     dark_brem_process_type = 11 # looked up in code
     recoil = None
     aprime = None
-    for id_p in e.SimParticles :
+    for id_p in event.SimParticles :
         if id_p.second.getProcessType() == dark_brem_process_type :
             if id_p.second.getPdgID() == 622 :
                 if aprime is not None :
-                    raise Exception(f'More than one A\' have been found in event {e.EventHeader.getEventNumber()}.')
+                    raise Exception(f'More than one A\' have been found in event {event.EventHeader.getEventNumber()}.')
                 aprime = id_p.second
             elif id_p.second.getPdgID() == 11 :
                 recoil = id_p.second
             else :
                 raise Exception(f'Particle {id_p.second.getPdgID()} produced by DB process.')
     if aprime is None :
-        raise Exception(f'No A\' found in {e.EventHeader.getEventNumber()}')
+        raise Exception(f'No A\' found in {event.EventHeader.getEventNumber()}')
     elif recoil is None :
-        raise Exception(f'No recoil electron found in {e.EventHeader.getEventNumber()}')
+        raise Exception(f'No recoil electron found in {event.EventHeader.getEventNumber()}')
 
     return recoil, aprime
 
