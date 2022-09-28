954d561ae24f97a939d91053831f81601510e469
tomeichlersmith <eichl008@umn.edu>
2022-09-28 08:39:10 -0500

    several small patches to plotting
    - don't use LaTeX font because it doesn't look good in paper
    - multi-line title and include it in legend title
    - silently scale MG by alpha-factor ratio
    - writeout pdf instead of default png
    - share one figure to avoid warnings from MPL
    - allow only plotting xsec

--- ana/plot.py
+++ ana/plot.py
@@ -5,15 +5,6 @@ import matplotlib.pyplot as plt
 import mplhep
 plt.style.use(mplhep.style.ROOT)
 
-# if the dvipng executable exists
-# then we can /probably/ use TeX to write the text
-import shutil
-if shutil.which('dvipng') is not None :
-    plt.rcParams.update({
-        'text.usetex' : True,
-        'font.family' : 'sans-serif',
-        'font.sans-serif' : ['Helvetica']})
-
 import uproot
 import numpy as np
 import pandas as pd
@@ -69,14 +60,14 @@ def bundle(data_dir, mg_dir) :
     """
     
     thin_tgt = (
-        ('4 GeV Electrons on 0.35 mm Tungsten',
+        ('4 GeV Electrons\non 0.35 mm Tungsten',
          { # electrons
           'G4DarkBreM' : read(el_beam,1e8,f'{data_dir}/ntuple_g4db_electron_tungsten_depthmm_0.35_mAMeV_100_events_50000_run_3000.root'),
           'DMG4' : read(el_beam,1e12,f'{data_dir}/ntuple_dmg4_electron_tungsten_depthmm_0.35_mAMeV_100_events_50000_run_1.root'),
           'MG/ME' : read(el_beam/1000.,5e5,f'{mg_dir}/electron_tungsten_MaxE_4.0_MinE_0.2_RelEStep_0.1_UndecayedAP_mA_0.1_run_3000/electron_tungsten_MaxE_4.0_MinE_0.2_RelEStep_0.1_UndecayedAP_mA_0.1_run_3000_IncidentEnergy_4.0_unweighted_events.lhe')
          }
         ),
-        ('100 GeV Muons on 100 mm Brass',
+        ('100 GeV Muons\non 100 mm Brass',
          { # muons
           'G4DarkBreM' : read(mu_beam,1e7,f'{data_dir}/ntuple_g4db_muon_brass_depthmm_100.0_mAMeV_1000_events_50000_run_3000.root'),
           'DMG4' : read(mu_beam,1e11,f'{data_dir}/ntuple_dmg4_muon_brass_depthmm_100.0_mAMeV_1000_events_50000_run_1.root'),
@@ -86,14 +77,14 @@ def bundle(data_dir, mg_dir) :
     )
     
     thick_tgt = (
-        ('4 GeV Electrons on 18mm Tungsten',
+        ('4 GeV Electrons\non 18mm Tungsten',
          {
           'G4DarkBreM' : read(el_beam,1e8,f'{data_dir}/ntuple_g4db_electron_tungsten_depthmm_18.0_mAMeV_100_events_50000_run_3000.root'),
           'DMG4' : read(el_beam,1e12,f'{data_dir}/ntuple_dmg4_electron_tungsten_depthmm_18.0_mAMeV_100_events_50000_run_1.root'),
           'Monoenergetic 4GeV MG/ME' : read(el_beam/1000.,5e5,f'{mg_dir}/electron_tungsten_MaxE_4.0_MinE_0.2_RelEStep_0.1_UndecayedAP_mA_0.1_run_3000/electron_tungsten_MaxE_4.0_MinE_0.2_RelEStep_0.1_UndecayedAP_mA_0.1_run_3000_IncidentEnergy_4.0_unweighted_events.lhe')
          }
         ),
-        ('100 GeV Muons on 2m Brass',
+        ('100 GeV Muons\non 2m Brass',
          {
           'G4DarkBreM' : read(mu_beam,1e7,f'{data_dir}/ntuple_g4db_muon_brass_depthmm_2000.0_mAMeV_1000_events_50000_run_3000.root'),
           'DMG4' : read(mu_beam,1e11,f'{data_dir}/ntuple_dmg4_muon_brass_depthmm_2000.0_mAMeV_1000_events_50000_run_1.root'),
@@ -102,14 +93,14 @@ def bundle(data_dir, mg_dir) :
         )
     )
 
-    na64 = ('100 GeV Electrons on 1mm Lead',
+    na64 = ('100 GeV Electrons\non 1mm Lead',
         {
           'G4DarkBreM' : read(el_beam,1e8,f'{data_dir}/ntuple_g4db_electron_lead_depthmm_1.0_mAMeV_100_events_50000_run_3000.root'),
           'DMG4' : read(el_beam,1e12,f'{data_dir}/ntuple_dmg4_electron_lead_depthmm_1.0_mAMeV_100_events_50000_run_1.root')
         }
         )
 
-    extra_thin = ('4 GeV Electrons on 0.035mm Tungsten',
+    extra_thin = ('4 GeV Electrons\non 0.035mm Tungsten',
         {
           'G4DarkBreM' : read(el_beam,1e8,f'{data_dir}/ntuple_g4db_electron_tungsten_depthmm_0.035_mAMeV_100_events_50000_run_3000.root'),
           'DMG4' : read(el_beam,1e12,f'{data_dir}/ntuple_dmg4_electron_tungsten_depthmm_0.035_mAMeV_100_events_50000_run_1.root'),
@@ -125,13 +116,12 @@ def single(data_packet, kinematic_variable, xlabel, file_name,
            hist_kwargs = {}, legend_kwargs = {}) :
     """Plot a single kinematic variable for the input data packet"""
     (title, data) = data_packet
-    fig, ((ax)) = plt.subplots()
-    fig.set_size_inches(11,8)
+    ((ax)) = plt.gcf().subplots()
+    plt.gcf().set_size_inches(11,8)
 
     ax.set_xlabel(xlabel)
     ax.set_yscale(yscale)
     ax.set_ylabel(ylabel)
-    ax.set_title(title)
     for name, df in data.items() :
         if drop_mg and 'MG/ME' in name :
             continue
@@ -142,10 +132,11 @@ def single(data_packet, kinematic_variable, xlabel, file_name,
                 weights = weights,
                 label = name, linewidth = 2.,
                 histtype = 'step', **hist_kwargs)
-    ax.legend(**legend_kwargs)
+    l = ax.legend(title=title, **legend_kwargs)
+    plt.setp(l.get_title(), multialignment='right')
     
     plt.savefig(file_name)
-          
+    plt.clf() 
 
 def side_by_side(data_packet, kinematic_variable, xlabel, file_name,
                  weight = True, ylabel = 'Weighted Event Fraction', yscale = 'log', 
@@ -155,8 +146,8 @@ def side_by_side(data_packet, kinematic_variable, xlabel, file_name,
     """Plot same kinematic variable side-by-side with a shared y-axis"""
     
     ((el_title,el_data),(mu_title,mu_data)) = data_packet
-    fig, ((el_ax, mu_ax)) = plt.subplots(ncols = 2, nrows = 1, sharey = 'row')
-    fig.set_size_inches(22,8)
+    ((el_ax, mu_ax)) = plt.gcf().subplots(ncols = 2, nrows = 1, sharey = 'row')
+    plt.gcf().set_size_inches(22,8)
     plt.subplots_adjust(hspace=0.3, wspace=0.)
     
     for ax in (el_ax,mu_ax) :
@@ -165,7 +156,6 @@ def side_by_side(data_packet, kinematic_variable, xlabel, file_name,
         
     el_ax.set_ylabel(ylabel)
     
-    el_ax.set_title(el_title)
     for name, df in el_data.items() :
         if drop_mg and 'MG/ME' in name :
             continue
@@ -176,9 +166,10 @@ def side_by_side(data_packet, kinematic_variable, xlabel, file_name,
                    weights = weights,
                    label = name, linewidth = 2.,
                    histtype = 'step', **el_kwargs, **hist_kwargs)
-    el_ax.legend(title='$m_{A\'}=0.1$ GeV', **legend_kwargs)
+    l = el_ax.legend(title=el_title+'\n$m_{A\'}=0.1$ GeV', **legend_kwargs)
+    plt.setp(l.get_title(), multialignment='right')
     
-    mu_ax.set_title(mu_title)
+    
     for name, df in mu_data.items() :
         if drop_mg and 'MG/ME' in name :
             continue
@@ -189,9 +180,11 @@ def side_by_side(data_packet, kinematic_variable, xlabel, file_name,
                    weights = weights,
                    label = name, linewidth = 2.,
                    histtype = 'step', **mu_kwargs, **hist_kwargs)
-    mu_ax.legend(title = '$m_{A\'}=1$ GeV', **legend_kwargs)
+    l = mu_ax.legend(title = mu_title+'\n$m_{A\'}=1$ GeV', **legend_kwargs)
+    plt.setp(l.get_title(), multialignment='right')
     
     plt.savefig(file_name)
+    plt.clf()
 
 def side_by_side_no_share(data_packet, kinematic_variable, xlabel, file_name,
                  weight = True, ylabel = 'Weighted Event Fraction', yscale = 'log', 
@@ -202,8 +195,8 @@ def side_by_side_no_share(data_packet, kinematic_variable, xlabel, file_name,
     """Plot same kinematic variable side-by-side without a shared y-axis"""
     
     ((el_title,el_data),(mu_title,mu_data)) = data_packet
-    fig, ((el_ax, mu_ax)) = plt.subplots(ncols = 2, nrows = 1)
-    fig.set_size_inches(22,8)
+    ((el_ax, mu_ax)) = plt.gcf().subplots(ncols = 2, nrows = 1)
+    plt.gcf().set_size_inches(22,8)
     
     for ax in (el_ax,mu_ax) :
         ax.set_xlabel(xlabel)
@@ -211,7 +204,6 @@ def side_by_side_no_share(data_packet, kinematic_variable, xlabel, file_name,
         
     el_ax.set_ylabel(ylabel)
     
-    el_ax.set_title(el_title)
     for name, df in el_data.items() :
         if drop_mg and 'MG/ME' in name :
             continue
@@ -224,9 +216,9 @@ def side_by_side_no_share(data_packet, kinematic_variable, xlabel, file_name,
                    histtype = 'step', **el_kwargs, **hist_kwargs)
     if el_ylim is not None :
         el_ax.set_ylim(el_ylim)
-    el_ax.legend(title='$m_{A\'}=0.1$ GeV', **legend_kwargs)
+    l = el_ax.legend(title=el_title+'\n$m_{A\'}=0.1$ GeV', **legend_kwargs)
+    plt.setp(l.get_title(), multialignment='right')
     
-    mu_ax.set_title(mu_title)
     for name, df in mu_data.items() :
         if drop_mg and 'MG/ME' in name :
             continue
@@ -239,32 +231,42 @@ def side_by_side_no_share(data_packet, kinematic_variable, xlabel, file_name,
                    histtype = 'step', **mu_kwargs, **hist_kwargs)
     if mu_ylim is not None :
         mu_ylim.set_ylim(mu_ylim)
-    mu_ax.legend(title = '$m_{A\'}=1$ GeV', **legend_kwargs)
+    l = mu_ax.legend(title = mu_title+'\n$m_{A\'}=1$ GeV', **legend_kwargs)
+    plt.setp(l.get_title(), multialignment='right')
     
     plt.savefig(file_name)
+    plt.clf()
 
-def xsec_plot(mg, others, title = None, ratio = True) :
-    fig, (raw, ratio) = plt.subplots(ncols=1,nrows=2,sharex='col', 
-                                     gridspec_kw = {'height_ratios' : [3,1]})
-    plt.subplots_adjust(hspace=0.)
+def xsec_plot(mg, others, file_name, title = None, ratio = True) :
+    (raw, ratio) = plt.gcf().subplots(ncols = 1, nrows = 2, 
+        sharex = 'col', gridspec_kw=dict(height_ratios = [3,1]))
+    plt.subplots_adjust(hspace=0)
 
-    raw.set_title(title)
-    raw.plot(mg['Energy [GeV]'], mg['Xsec [pb]'], label='MG')
-    ratio.plot(mg['Energy [GeV]'], [1. for i in range(len(mg['Energy [GeV]']))])
+    mg_x = mg['Energy [GeV]']
+    mg_y = mg['Xsec [pb]']*(127.9/137)**3
+
+    raw.plot(mg_x, mg_y,marker='.', linewidth=0, label='MG')
+    ratio.plot(mg_x, [1. for x in mg_x], marker='.', linewidth=0)
     for name, data in others :
-        x = data['Energy [MeV]']/1000.
         y = data['Xsec [pb]']
-        raw.plot(x, y, label=name)
-        data_interp = scipy.interpolate.interp1d(x, y)
-        data_at_mge = [data_interp(e) for e in mg['Energy [GeV]']]
-        ratio.plot(mg['Energy [GeV]'], data_at_mge/mg['Xsec [pb]'])
+        if 'All MG' in name :
+            x = data['Energy [GeV]']
+            raw.plot(x,y*(127.9/137)**3,label=name,marker='.', linewidth=0)
+        else :
+            x = data['Energy [MeV]']/1000.
+            raw.plot(x, y, label=name)
+            data_interp = scipy.interpolate.interp1d(x, y)
+            data_at_mge = [data_interp(e) for e in mg_x]
+            ratio.plot(mg_x, data_at_mge/mg_y, marker='.', linewidth=0)
 
     raw.set_ylabel('Total Cross Section [pb]')
-    raw.legend()
+    l = raw.legend(title=title)
+    plt.setp(l.get_title(), multialignment='right')
 
     ratio.set_ylabel('Ratio to MG')
     ratio.set_xlabel('Incident Lepton Energy [GeV]')
-    ratio.axhline((127/137)**3, color='gray')
+    plt.savefig(file_name)
+    plt.clf()
 
 def average_mg(all_samples) :
     """
@@ -285,6 +287,7 @@ def main() :
     parser.add_argument('data_dir',help='Directory data is in')
     parser.add_argument('--out_dir',help='Directory to put plots (Default: data_dir)')
     parser.add_argument('--mg_dir',help='Directory MG libraries are in',default='dblib')
+    parser.add_argument('--xsec-only', help='Only print xsec plots',action='store_true')
     
     arg = parser.parse_args()
     
@@ -295,19 +298,24 @@ def main() :
 
     xsec_plot(average_mg('data/mg/mu_xsec.csv'), [
             ('DMG4 WW', pd.read_csv(f'{arg.data_dir}/dmg4_mu_xsec.csv')),
-            ('G4DB WW', pd.read_csv(f'{arg.data_dir}/g4db_mu_xsec.csv'))
+            ('G4DB WW', pd.read_csv(f'{arg.data_dir}/g4db_mu_xsec.csv')),
+            ('All MG', pd.read_csv('data/mg/mu_xsec.csv'))
           ],
+        f'{arg.out_dir}/mu_xsec.pdf',
         title = 'Muons on Copper')
-    plt.savefig(f'{arg.out_dir}/mu_xsec')
 
     xsec_plot(average_mg('data/mg/el_xsec.csv'), [
             ('DMG4 IWW + Log Approx + K factors', 
               pd.read_csv(f'{arg.data_dir}/dmg4_el_xsec.csv').sort_values('Energy [MeV]')),
             #('G4DB WW', pd.read_csv('data/dev/el_xsec.csv')),
-            ('G4DB IWW', pd.read_csv(f'{arg.data_dir}/g4db_el_xsec.csv'))
+            ('G4DB IWW', pd.read_csv(f'{arg.data_dir}/g4db_el_xsec.csv')),
+            ('All MG', pd.read_csv('data/mg/el_xsec.csv'))
           ],
+         f'{arg.out_dir}/el_xsec.pdf',
         title = 'Electrons on Tungsten')
-    plt.savefig(f'{arg.out_dir}/el_xsec')
+
+    if arg.xsec_only :
+        return
 
     # load data into memory bundles
     thin_tgt, thick_tgt, na64, extra_thin = bundle(arg.data_dir, arg.mg_dir)
@@ -316,66 +324,66 @@ def main() :
     single(na64, 'recoil_angle', 'Lepton Recoil Angle [rad]',
            #el_ylim = (7e-4,2),
            hist_kwargs = {'range' : (0,2), 'bins' : 50},
-           file_name = arg.out_dir+'/100GeV-electron-lead-recoil-angle')
+           file_name = arg.out_dir+'/100GeV-electron-lead-recoil-angle.pdf')
     single(na64, 'visible_energy_frac', 'Visible Energy Fraction of Beam',
            hist_kwargs = {'range' : (0,1), 'bins' : 50},
-           file_name = arg.out_dir+'/100GeV-electron-lead-visible-energy')
+           file_name = arg.out_dir+'/100GeV-electron-lead-visible-energy.pdf')
     single(na64, 'visible_energy_frac', 'Visible Energy Fraction of Beam', 
            ylabel = 'Fraction Events Below Energy Cut',
            yscale = 'linear',
            hist_kwargs = {'range' : (0,1), 'bins': 50, 'cumulative' : True},
            legend_kwargs = {'loc':'lower center'},
-           file_name = arg.out_dir+'/100GeV-electron-lead-visible-energy-cumulative')
+           file_name = arg.out_dir+'/100GeV-electron-lead-visible-energy-cumulative.pdf')
     single(na64, 'incident_kinetic_energy_GeV', 
            'Lepton Kinetic Energy Prior to DB [GeV]',
            hist_kwargs = {'range' : (0.,100.), 'bins' : 50 },
            legend_kwargs = {'loc' : 'upper left'},
            drop_mg = True,
-           file_name = arg.out_dir+'/100GeV-electron-lead-incident-energy')
+           file_name = arg.out_dir+'/100GeV-electron-lead-incident-energy.pdf')
     single(na64, 'relative_weight', 'Event Weight',
            weight = False, 
            hist_kwargs = {'range':(1,1.2),'bins':50},
            drop_mg = True,
-           file_name = arg.out_dir+'/100GeV-electron-lead-event-weight')
+           file_name = arg.out_dir+'/100GeV-electron-lead-event-weight.pdf')
 
     single(extra_thin, 'recoil_angle', 'Lepton Recoil Angle [rad]',
            #el_ylim = (7e-4,2),
            hist_kwargs = {'range' : (0,2), 'bins' : 50},
-           file_name = arg.out_dir+'/4GeV-electron-extra-thin-recoil-angle')
+           file_name = arg.out_dir+'/4GeV-electron-extra-thin-recoil-angle.pdf')
     single(extra_thin, 'visible_energy_frac', 'Visible Energy Fraction of Beam',
            hist_kwargs = {'range' : (0,1), 'bins' : 50},
-           file_name = arg.out_dir+'/4GeV-electron-extra-thin-visible-energy')
+           file_name = arg.out_dir+'/4GeV-electron-extra-thin-visible-energy.pdf')
     single(extra_thin, 'visible_energy_frac', 'Visible Energy Fraction of Beam', 
            ylabel = 'Fraction Events Below Energy Cut',
            yscale = 'linear',
            hist_kwargs = {'range' : (0,1), 'bins': 50, 'cumulative' : True},
-           legend_kwargs = {'loc':'lower center'},
-           file_name = arg.out_dir+'/4GeV-electron-extra-thin-visible-energy-cumulative')
+           legend_kwargs = {'loc':'lower right', 'framealpha': 0.8},
+           file_name = arg.out_dir+'/4GeV-electron-extra-thin-visible-energy-cumulative.pdf')
     single(extra_thin, 'incident_kinetic_energy_GeV', 
            'Lepton Kinetic Energy Prior to DB [GeV]',
            hist_kwargs = {'range' : (0.,100.), 'bins' : 50 },
            legend_kwargs = {'loc' : 'upper left'},
            drop_mg = True,
-           file_name = arg.out_dir+'/4GeV-electron-extra-thin-incident-energy')
+           file_name = arg.out_dir+'/4GeV-electron-extra-thin-incident-energy.pdf')
     single(extra_thin, 'relative_weight', 'Event Weight',
            weight = False, 
            hist_kwargs = {'range':(1,1.2),'bins':50},
            drop_mg = True,
-           file_name = arg.out_dir+'/4GeV-electron-extra-thin-event-weight')
+           file_name = arg.out_dir+'/4GeV-electron-extra-thin-event-weight.pdf')
 
     side_by_side_no_share(thin_tgt, 'recoil_angle', 'Lepton Recoil Angle [rad]',
                  el_ylim = (7e-4,2),
                  hist_kwargs = {'range' : (0,2), 'bins' : 50},
-                 file_name = arg.out_dir+'/thin-recoil-angle')
+                 file_name = arg.out_dir+'/thin-recoil-angle.pdf')
     side_by_side(thin_tgt, 'visible_energy_frac', 'Visible Energy Fraction of Beam',
                  hist_kwargs = {'range' : (0,1), 'bins' : 50},
-                 file_name = arg.out_dir+'/thin-visible-energy')
+                 file_name = arg.out_dir+'/thin-visible-energy.pdf')
     side_by_side(thin_tgt, 'visible_energy_frac', 'Visible Energy Fraction of Beam', 
                  ylabel = 'Fraction Events Below Energy Cut',
                  yscale = 'linear',
                  hist_kwargs = {'range' : (0,1), 'bins': 50, 'cumulative' : True},
-                 legend_kwargs = {'loc':'lower center'},
-                 file_name = arg.out_dir+'/thin-visible-energy-cumulative')
+                 legend_kwargs = {'loc':'lower right', 'framealpha' : 0.8},
+                 file_name = arg.out_dir+'/thin-visible-energy-cumulative.pdf')
     side_by_side(thin_tgt, 'incident_kinetic_energy_GeV', 
                  'Lepton Kinetic Energy Prior to DB [GeV]',
                  el_kwargs = {'range' : (0.,4.)},
@@ -383,25 +391,25 @@ def main() :
                  legend_kwargs = {'loc' : 'upper left'},
                  hist_kwargs = { 'bins' : 50 },
                  drop_mg = True,
-                 file_name = arg.out_dir+'/thin-incident-energy')
+                 file_name = arg.out_dir+'/thin-incident-energy.pdf')
     side_by_side(thin_tgt, 'relative_weight', 'Event Weight',
                  weight = False, 
                  hist_kwargs = {'range':(1,1.2),'bins':50},
                  drop_mg = True,
-                 file_name = arg.out_dir+'/thin-event-weight')
+                 file_name = arg.out_dir+'/thin-event-weight.pdf')
     
     side_by_side(thick_tgt, 'recoil_angle', 'Lepton Recoil Angle [rad]',
                  hist_kwargs = {'range' : (0,2), 'bins' : 50},
-                 file_name = arg.out_dir+'/thick-recoil-angle')
+                 file_name = arg.out_dir+'/thick-recoil-angle.pdf')
     side_by_side(thick_tgt, 'visible_energy_frac', 'Visible Energy Fraction of Beam',
                  hist_kwargs = {'range' : (0,1), 'bins' : 50},
-                 file_name = arg.out_dir+'/thick-visible-energy')
+                 file_name = arg.out_dir+'/thick-visible-energy.pdf')
     side_by_side(thick_tgt, 'visible_energy_frac', 'Visible Energy Fraction of Beam', 
                  ylabel = 'Fraction Events Below Energy Cut',
                  yscale = 'linear',
                  hist_kwargs = {'range' : (0,1), 'bins': 50, 'cumulative' : True},
                  legend_kwargs = {'loc':'lower center'},
-                 file_name = arg.out_dir+'/thick-visible-energy-cumulative')
+                 file_name = arg.out_dir+'/thick-visible-energy-cumulative.pdf')
     side_by_side(thick_tgt, 'incident_kinetic_energy_GeV', 
                  'Lepton Kinetic Energy Prior to DB [GeV]',
                  el_kwargs = {'range' : (0.,4.)},
@@ -409,13 +417,13 @@ def main() :
                  legend_kwargs = {'loc' : 'upper left'},
                  hist_kwargs = { 'bins' : 50 },
                  drop_mg = True,
-                 file_name = arg.out_dir+'/thick-incident-energy')
+                 file_name = arg.out_dir+'/thick-incident-energy.pdf')
     side_by_side(thick_tgt, 'relative_weight', 'Event Weight',
                  weight = False,
                  el_kwargs = {'range':(1,10), 'bins':100},
                  mu_kwargs = {'range':(1,1.2),'bins':50},
                  drop_mg = True,
-                 file_name = arg.out_dir+'/thick-event-weight')
+                 file_name = arg.out_dir+'/thick-event-weight.pdf')
     
 if __name__ == '__main__' :
     main()
