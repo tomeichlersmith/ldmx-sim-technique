9c189adc5f8cbe5fd8fb644d28b63adf4cad913b
tomeichlersmith <eichl008@umn.edu>
2023-01-16 12:06:09 -0600

    inset the ratio and not the raw, leave inset in same location

--- ana/xsec.py
+++ ana/xsec.py
@@ -43,14 +43,18 @@ def xsec_plot(mg_csv, others, file_name,
     ratio.plot(mg_x, [1. for x in mg_x], marker='.', markersize=0, linewidth=0, color='tab:green')
     
     if inset_xlim is not None :
+        # we position the inset in the raw axes
+        # and then mark it relative to the ratio axes we are zooming in on
         inset = plt.axes([0,0,1,1])
         # left edge, bottom edge, width, height
         inset.set_axes_locator(InsetPosition(raw, [0.50,0.10,0.45,0.45]))
-        mark_inset(raw, inset, loc1=2, loc2=4, fc='none', ec='0.5')
+        mark_inset(ratio, inset, loc1=2, loc2=4, fc='none', ec='0.5')
 
         selection = (mg_x > inset_xlim[0])&(mg_x < inset_xlim[1])
-        inset.plot(mg_x[selection], mg_y[selection], linewidth=0, label='MG/ME',
-                   marker='o', markersize=10, markerfacecolor='none', markeredgecolor='tab:green')
+        inset.plot(mg_x[selection], [1 for x in mg_x[selection]], 
+                   linewidth=0, label='MG/ME', 
+                   #marker='o', markersize=10, markerfacecolor='none', 
+                   markeredgecolor='tab:green')
 
     for name, data, style in others :
         y = data['Xsec [pb]']
@@ -59,12 +63,15 @@ def xsec_plot(mg_csv, others, file_name,
         data_interp = scipy.interpolate.interp1d(x, y)
         in_range = (mg_x >= x.min())&(mg_x <= x.max())#&(mg_x > 100)
         data_at_mge = [data_interp(e) for e in mg_x[in_range]]
-        ratio.plot(mg_x[in_range], data_at_mge/mg_y[in_range], 
+        ratio_vals = data_at_mge/mg_y[in_range]
+        ratio.plot(mg_x[in_range], ratio_vals,
             marker='.', markersize=15, linewidth=0, **style)
 
         if inset_xlim is not None :
-            sl = (x > inset_xlim[0])&(x < inset_xlim[1])
-            inset.plot(x[sl], y[sl], label=name, linewidth=2, **style)
+            sl = in_range&(mg_x > inset_xlim[0])&(mg_x < inset_xlim[1])
+            inset.plot(mg_x[sl], ratio_vals[sl], 
+                marker='.', markersize=15, linewidth=0,
+                label=name, **style)
 
     raw.set_ylabel('Total Cross Section / $\epsilon^2$ [pb]')
     raw.set_ylim(ymax=ymax)
@@ -109,7 +116,7 @@ def main() :
               dict(color='tab:blue')),
           ],
          f'{arg.out_dir}/el_xsec.pdf',
-        inset_xlim=(0,10), ymax=1.1e10,
+        inset_xlim=(0,5), ymax=1.1e10,
         xlabel = 'Incident Electron Energy [GeV]',
         title = 'Electrons on Tungsten $m_{A\'} = 0.1$ GeV')
 
@@ -122,7 +129,7 @@ def main() :
               dict(color='tab:red')),
           ],
          f'{arg.out_dir}/el_xsec_appendix.pdf',
-        inset_xlim = (0,10), legend_overhead=True,
+        inset_xlim = (0,5), legend_overhead=True,
         xlabel = 'Incident Electron Energy [GeV]',
         title = 'Electrons on Tungsten $m_{A\'} = 0.1$ GeV')
 
@@ -132,7 +139,7 @@ def main() :
               dict(color='tab:blue')),
           ],
         f'{arg.out_dir}/mu_xsec.pdf',
-        inset_xlim=(0,200),
+        inset_xlim=(0,100),
         xlabel = 'Incident Muon Energy [GeV]', ymax=5.5e6,
         title = 'Muons on Copper $m_{A\'} = 1.0$ GeV')
 
@@ -146,7 +153,7 @@ def main() :
           ],
         f'{arg.out_dir}/mu_xsec_appendix.pdf',
         legend_overhead=True,
-        inset_xlim=(0,200), ymax=5.5e6,
+        inset_xlim=(0,100), ymax=5.5e6,
         xlabel = 'Incident Muon Energy [GeV]',
         title = 'Muons on Copper $m_{A\'} = 1.0$ GeV')
 
